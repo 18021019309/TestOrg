7df20d1290396cdf9e815d9b29c2669f01d1058c
王天琪 <2586172363@qq.com>
2020-06-18 20:57:45 +0800

    初始化

---
+++ Controllers/TestController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace StudyTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class TestController : ControllerBase
+    {
+        public class TestModel
+        {
+            //[FromHeader]
+            public string Accept { get;set;}
+            [FromHeader]
+            public string Host { get;set;}
+        }
+
+        [HttpGet]
+        public IActionResult GetQuery([FromQuery]TestModel testModel)
+        {
+            return Ok(testModel);
+        }
+
+        [HttpPost]
+        public IActionResult PostData(int a,int b)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetBody([Bind]TestModel testModel)
+        {
+            return Ok(testModel);
+        }
+        [HttpGet]
+        public IActionResult GetForm([FromForm]TestModel testModel)
+        {
+            return Ok(testModel);
+        }
+
+        [HttpGet]
+        public IActionResult GetHeader([FromHeader]TestModel testModel)
+        {
+            return Ok(testModel);
+        }
+    }
+}
